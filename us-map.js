b74f0df2a7a0566da8be76ff3f166ea46655a195
abhi123456781 <72471412+abhi123456781@users.noreply.github.com>
2025-07-29 01:02:07 -0400

    Update us-map.js

--- us-map.js
+++ us-map.js
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'VA': { x: 0.835, y: 0.455 },  // Virginia
         'FL': { x: 0.835, y: 0.800 },  // Florida
         'GA': { x: 0.800, y: 0.650 },  // Georgia
-        'OH': { x: 0.735, y: 0.265 },  // Ohio
+        'OH': { x: 0.765, y: 0.380 },  // Ohio
         'MI': { x: 0.725, y: 0.255 },  // Michigan
         'SC': { x: 0.825, y: 0.575 },  // South Carolina
         'NE': { x: 0.485, y: 0.265 },  // Nebraska
